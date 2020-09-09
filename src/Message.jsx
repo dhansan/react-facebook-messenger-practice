6ad21e18d67b0277559f42d0262977c5c9c8fd8c
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-09-09 22:40:19 +0700

    Add Material UI dependencies

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Card, CardContent, Typography } from '@material-ui/core';
+import './Message.css';
 
-function Message(props) {
+function Message({ message, username }) {
+  const isUser = username === message.username;
+
   return (
     <div>
-      <h2>{props.text}</h2>
+      <Card className={`message ${isUser && 'message_user'}`}>
+        <CardContent>
+          <Typography color="white" varian="h5" component="h2">
+            {message.username}: {message.text}
+          </Typography>
+        </CardContent>
+      </Card>
+      <h2></h2>
     </div>
   );
 }

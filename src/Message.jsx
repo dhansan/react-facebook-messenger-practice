26215e53721d787d6ea74485d6786516ff1aecc9
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-09-11 10:47:39 +0700

    set HOC forwardRef and ref

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { Card, CardContent, Typography } from '@material-ui/core';
-import './Message.css';
+import './Message.scss';
 
-function Message({ message, username }) {
+const Message = forwardRef(({ message, username }, ref) => {
   const isUser = username === message.username;
 
   return (
-    <div>
-      <Card className={`message ${isUser && 'message_user'}`}>
+    <div ref={ref} className={`message ${isUser && 'message__user'}`}>
+      <Card className={isUser ? 'message__userCard' : 'message__guestCard'}>
         <CardContent>
           <Typography color="white" varian="h5" component="h2">
-            {message.username}: {message.text}
+            {message.username}: {message.message}
           </Typography>
         </CardContent>
       </Card>
       <h2></h2>
     </div>
   );
-}
+});
 
 export default Message;

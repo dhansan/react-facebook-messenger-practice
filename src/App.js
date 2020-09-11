e087be238b557fb65b9d99bf428d9073c337f3d6
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-09-11 10:49:52 +0700

    set orderby timestamp and desc firebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import firebase from 'firebase';
 import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import './App.css';
 import Message from './Message';
+import db from './firebase';
 
 function App() {
   const [input, setInput] = useState('');
-  const [messages, setMessages] = useState([
-    { username: 'Dhan', text: 'haloo' },
-    { username: 'Jani', text: 'hey bro' },
-  ]);
+  const [messages, setMessages] = useState([]);
   const [username, setUsername] = useState('');
+
+  // useState = variable in REACT
+  // useEffect = run code on a condition in REACT (only running a page load)
+
+  useEffect(() => {
+    db.collection('messages')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot((snapshot) => {
+        setMessages(snapshot.docs.map((doc) => doc.data()));
+      });
+  }, []);
 
   useEffect(() => {
     setUsername(prompt('Enter your name :'));
@@ -17,7 +27,13 @@ function App() {
 
   const sendMessage = (event) => {
     event.preventDefault();
-    setMessages([...messages, { username: username, text: input }]);
+
+    db.collection('messages').add({
+      message: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    // setMessages([...messages, { username: username, text: input }]);
     setInput('');
   };
 
